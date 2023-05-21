d6a641b96949d5083b11fc2636aab9d51f8ec40c
Andrea <andrea.ciambriello@hotmail.com>
2023-05-21 18:02:12 +0200

    backup-Main terzo commit, WrapperClass

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main (String[] args) {
 
-        Stringhe metodiStringhe = new Stringhe();
-        metodiStringhe.usoStringhe();
+        WrapperClass reference = new WrapperClass();
+        reference.usoWrapperClass();
     }
 }
