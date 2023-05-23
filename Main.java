5881d049161f6cf4f5b0fa4a34e4e9db74d7ed39
Andrea <andrea.ciambriello@hotmail.com>
2023-05-23 21:05:26 +0200

    backup-Main quinto commit, Metodi

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main (String[] args) {
 
-        ArrayListe listaDiArray = new ArrayListe();
-        listaDiArray.usoArrayListe();
+        Metodi usoDeiMetodi = new Metodi();
+
+        //stiamo richiamando il metodo faiPasta, in questo caso abbiamo bisogno di una istanza della classe perchè il metodo è definito li
+        usoDeiMetodi.preparaPasta();
+        //richiamo un altro metodo come sopra e gli passo un valore come parametro
+        usoDeiMetodi.preparaQualcosa("pizza");
+        //richiamo un altro metodo che prende due int come parametri. La particolarità di questo metodo è che per essere chiamato non ha
+        //bisogno di una istanza della classe, ma richiamiamo la classe stessa, per via della keyword static
+        Metodi.addizione(2, 3); 
+
+        Metodi.moltiValori(3, 4, 6, 7, 9, 15);    //essendo un varargs, gli posso passare quanti valori preferisco
     }
 }
